1d845e582b8d99b3718d9547a4af2328805dffd3
Charlie <charlie@unedited.site>
2024-04-02 20:28:51 +0200

    Update app.js

--- server/database/app.js
+++ server/database/app.js
@@ -34,7 +34,7 @@ try {
 
 // Express route to home
 app.get('/', async (req, res) => {
-    res.send("Welcome to the Mongoose API")
+    res.send("Welcome to the Mongoose API");
 });
 
 // Express route to fetch all reviews
@@ -63,7 +63,7 @@ app.get('/fetchDealers', async (req, res) => {
         const documents = await Dealerships.find();
         res.json(documents)
     } catch (error) {
-        res.status(500).json({ error: 'Error fetching documents'})
+        res.status(500).json({ error: 'Error fetching documents'});
     }
 });
 
@@ -71,9 +71,9 @@ app.get('/fetchDealers', async (req, res) => {
 app.get('/fetchDealers/:state', async (req, res) => {
     try {
         const documents = await Dealerships.find({ state: req.params.state});
-        res.json(documents)
+        res.json(documents);
     } catch (error) {
-        res.status(500).json({ error: 'Error fetching documents'})
+        res.status(500).json({ error: 'Error fetching documents'});
     }
 });
 
@@ -81,28 +81,28 @@ app.get('/fetchDealers/:state', async (req, res) => {
 app.get('/fetchDealer/:id', async (req, res) => {
     try {
         const documents = await Dealerships.find({ id: req.params.id });
-        res.json(documents)
+        res.json(documents);
     } catch (error) {
-        res.status(500).json({ error: 'Error fetching documents'})
+        res.status(500).json({ error: 'Error fetching documents'});
     }
 });
 
 //Express route to insert review
 app.post('/insert_review', express.raw({ type: '*/*' }), async (req, res) => {
   data = JSON.parse(req.body);
-  const documents = await Reviews.find().sort( { id: -1 } )
-  let new_id = documents[0]['id']+1
+  const documents = await Reviews.find().sort( { id: -1 } );
+  let new_id = documents[0].id+1;
 
   const review = new Reviews({
 		"id": new_id,
-		"name": data['name'],
-		"dealership": data['dealership'],
-		"review": data['review'],
-		"purchase": data['purchase'],
-		"purchase_date": data['purchase_date'],
-		"car_make": data['car_make'],
-		"car_model": data['car_model'],
-		"car_year": data['car_year'],
+		"name": data.name,
+		"dealership": data.dealership,
+		"review": data.review,
+		"purchase": data.purchase,
+		"purchase_date": data.purchase_date,
+		"car_make": data.car_make,
+		"car_model": data.car_model,
+		"car_year": data.car_year,
 	});
 
   try {
