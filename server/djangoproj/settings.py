75e4ba5cf3b20a0c0cfd4f8cb41f90def451b94a
Charlie <charlie@unedited.site>
2024-04-02 20:14:44 +0200

    Update settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -102,7 +102,7 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME':
-        'django.contrib.auth.password_' \+
+        'django.contrib.auth.password_' \
         'validation.UserAttributeSimilarityValidator',
     },
     {
