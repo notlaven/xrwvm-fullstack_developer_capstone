ef161696581e4bb8106e7e30cf26714a549d9a60
Charlie <charlie@unedited.site>
2024-04-02 20:11:51 +0200

    Update settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -30,9 +30,14 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     'localhost',
-    'https://tjallingvana-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
+    'https://tjallingvana-8000.theiadockernext-' \+
+    '0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
+]
+
 CSRF_TRUSTED_ORIGINS = [
-    'https://tjallingvana-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
+    'https://tjallingvana-8000.theiadockernext-' \+
+    '0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
+]
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
@@ -97,7 +102,8 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME':
-        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'django.contrib.auth.password_' \+
+        'validation.UserAttributeSimilarityValidator',
     },
     {
         'NAME':
