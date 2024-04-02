c4493801f397b69364b42c1fd1686f7b84ea3b2d
Charlie <charlie@unedited.site>
2024-04-02 09:49:34 +0200

    Update restapis.py

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -14,12 +14,11 @@ sentiment_analyzer_url = os.getenv(
 
 def get_request(endpoint, **kwargs):
     params = ""
-    
-    if(kwargs):
-        for key,  value in kwargs.items():
+    if  (kwargs):
+        for  key, value  in  kwargs.items():
             params  =  params+key+"="+value+"&"
 
-    request_url  =  backend_url+endpoint+"?"+params
+    request_url = backend_url+endpoint+"?"+params
 
     print("GET from {} ".format(request_url))
     try:
